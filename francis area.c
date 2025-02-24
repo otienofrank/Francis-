718e9098b4ffc0aa5f25cd5b1f9c96ba7e2240b1
otienofrank <otienofrank2023@gmail.com>
2025-02-24 08:49:10 -0600

    Add files via upload

---
+++ francis area.c
@@ -0,0 +1,21 @@
+\/*
+author:kigacha Nancy Gakii
+reg no:d33-2424-2023
+Program:to find area of rectangle 
+date:20/2/2025
+*/
+#include<stdio.h>//printf()
+int main(){
+// variable decelerations and initialization;
+int l;
+int w;
+int area;
+printf("program to multiply 2 numbers: \n");
+    printf("enter the first number:\n");
+    scanf("%d",&l);
+    printf("enter the second number:\n");
+    scanf("%d",&w);
+    area=l*w;
+    printf ("the area is %d",area);
+    return 0;
+}
